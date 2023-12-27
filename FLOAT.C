c70a1a2372c1f28bae2506a4b74a6ac0e3c4493e
dharmi9397 <patelzerox2008@gemail.com>
2023-12-27 15:58:06 +0530

    dharmee

---
+++ FLOAT.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+ float x=46.54;
+ clrscr();
+ printf("%f",x);
+ getch();
+}
---
+++ SCANF.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+ int a;
+ int b;
+ clrscr();
+ printf("my name :-");
+ scanf("%d",&a);
+ printf("my fav letter :-p");
+ scanf("%d",&b);
+ getch();
+
+}
